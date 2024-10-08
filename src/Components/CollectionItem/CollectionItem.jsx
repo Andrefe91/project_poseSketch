b45c84b274a967b8e4627d8c4b4a418d8312baf3
Andres Muñoz <andrefe91@gmail.com>
2024-10-08 01:24:47 -0500

    image-details style

--- src/Components/CollectionItem/CollectionItem.jsx
+++ src/Components/CollectionItem/CollectionItem.jsx
@@ -6,20 +6,28 @@ import "./collectionitem.css"
 
 
 function CollectionItem({imageFile, number}) {
-    const imageName = imageFile.name.toLowerCase();
-    const imageNameCapitalize = imageName.split(".")[0][0].toUpperCase() + imageName.split(".")[0].slice(1);
+    let imageName = imageFile.name.split(".")[0][0].toUpperCase() + imageFile.name.toLowerCase().split(".")[0].slice(1);
+
+    if (imageName.length > 15) {
+        imageName = imageName.slice(0, 15).trim() + "...";  //Limit the image name to 10 characters and add ellipsis if necessary.  //Limit the image name to 10 characters and add ellipsis if necessary.  //Limit the image name to 10 characters and add ellipsis if necessary.  //Limit the image name to 10 characters and add ellipsis if necessary.  //Limit the image name to 10 characters and add ellipsis if necessary.  //Limit the image name to 10 characters and add ellipsis if necessary.  //Limit the image name to 10 characters and add ellipsis if necessary.  //Limit the image name to 10 characters and add ellipsis if necessary.  //Limit the image name to 10 characters and add ellipsis if necessary.  //Limit the image name to 10 characters and add ellipsis if necessary.  //Limit the image name to 10 characters and add
+    }
+
 
     return (
         <>
             <div className="collection-item">
                 <div className="image-display">
-                    <img src={URL.createObjectURL(imageFile)} alt={imageNameCapitalize} />
+                    <img src={URL.createObjectURL(imageFile)} alt={imageName} />
                 </div>
 
-                <div className="image-details">
-                    <p>{imageNameCapitalize}</p>
-                    <p>#{number + 1}</p>
+                <div className="background">
+                    <div className="image-details">
+                        <p>{imageName}</p>
+                        <p>#{number + 1}</p>
+                    </div>
+
                 </div>
+
             </div>
         </>
     )
